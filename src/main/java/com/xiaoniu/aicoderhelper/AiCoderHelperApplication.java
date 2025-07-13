0b9a8af252a40ab912752b42195ad482e0aecf57
小牛667 <578812414@qq.com>
2025-07-13 11:30:18 +0800

    first commit

---
+++ src/main/java/com/xiaoniu/aicoderhelper/AiCoderHelperApplication.java
@@ -0,0 +1,13 @@
+package com.xiaoniu.aicoderhelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiCoderHelperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiCoderHelperApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xiaoniu/aicoderhelper/AiCoderHelperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiaoniu.aicoderhelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiCoderHelperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
